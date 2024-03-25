d8a9e9a4155d78b80657eea97a282ad80fa36753
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-25 14:34:38 +0100

    :sparkles: adding div with image and text content

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './styles/style.css'
 
 import { generateHTMLHeader } from './generateHeader'
+import { generateSiteContent } from './generateSiteContent'
 
 export let app = document.querySelector('#app') as HTMLDivElement || null
 
 generateHTMLHeader()
+generateSiteContent()
