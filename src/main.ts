7b6ec8bcac53d96bf50beaae32adbec83527a622
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-26 11:45:22 +0100

    :sparkles: Adding footer with icon

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './styles/style.css'
 import { generateHTMLHeader } from './generateHeader'
 import { generateHTMLCooking } from './generateCookingDiv'
 import { generateHTMLFavoriteCooking } from './generateFavoriteCooking'
+import { generateFooter } from './generateFooter'
 
 export let app = document.querySelector('#app') as HTMLDivElement || null
 
 generateHTMLHeader()
 generateHTMLCooking()
 generateHTMLFavoriteCooking()
+generateFooter()
