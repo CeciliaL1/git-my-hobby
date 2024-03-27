a67ffa83e29714ecde122f102b743ebd805e1819
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-27 14:19:44 +0100

    :sparkles: adding  ul-li to menu

--- src/openMenu.ts
+++ src/openMenu.ts
@@ -1,9 +1,22 @@
-import { generateHTMLHeader } from "./generateHeader";
+
 import { app } from "./main";
 
 
 export const openMenu = (menuContainer: HTMLDivElement):void => {
-console.log('open meny')
 
+    let menuNav = document.createElement('nav');
+
+    let menuUl = document.createElement('ul')
+
+    let menuItems = ['Home', 'My Favorite Ingrediens', 'My Cookware', 'Contact'] 
+
+    menuItems.forEach(item => {
+        let menuLi = document.createElement('li')
+        menuLi.textContent = item;
+        menuUl.append(menuLi)
+    })
+
+    menuNav.appendChild(menuUl)
+    menuContainer.appendChild(menuNav)
     app.appendChild(menuContainer)
 }
